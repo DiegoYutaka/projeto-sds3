e4d9a9589235007e9b2f59d7d693e610160fc27e
Diego Miyashita <diegoyutaka3@gmail.com>
2021-09-09 23:32:25 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import NavBar from "components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-primary">Hello World!</h1> 
+      <h1 className="text-primary">Hello World!</h1>
+      <NavBar/>
     </div>
   );
 }
